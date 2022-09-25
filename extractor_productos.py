cbf60a081b2a3236dffbfb1c295746e9789f75f8
jairoparedes <hiro.paredes.candia@gmail.com>
2022-09-24 23:35:43 -0300

    extractor de productos

---
+++ extractor_productos.py
@@ -0,0 +1,142 @@
+# web scraping con python
+# https://distribuidoranico.cl/todos-los-productos/page/2/?v=cda73baae416
+# <a href="https://distribuidoranico.cl/producto/alfajor-bon-o-bon-40-g/?v=cda73baae416" class="woocommerce-LoopProduct-link woocommerce-loop-product__link"><img width="300" height="375" src="https://distribuidoranico.cl/wp-content/uploads/2021/06/Captura-de-pantalla-234-300x375.png" data-lazy-type="image" data-src="https://distribuidoranico.cl/wp-content/uploads/2021/06/Captura-de-pantalla-234-300x375.png" class="attachment-woocommerce_thumbnail size-woocommerce_thumbnail lazy-loaded" alt=""><noscript><img width="300" height="375"   alt="" data-src="https://distribuidoranico.cl/wp-content/uploads/2021/06/Captura-de-pantalla-234-300x375.png" class="attachment-woocommerce_thumbnail size-woocommerce_thumbnail lazyload" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" /><noscript><img width="300" height="375" src="https://distribuidoranico.cl/wp-content/uploads/2021/06/Captura-de-pantalla-234-300x375.png" class="attachment-woocommerce_thumbnail size-woocommerce_thumbnail" alt="" /></noscript></a>
+
+# detalle producto
+# <div class="woocommerce-product-gallery woocommerce-product-gallery--with-images woocommerce-product-gallery--columns-4 images" data-columns="4" style="opacity: 1; transition: opacity 0.25s ease-in-out 0s;"><a href="#" class="woocommerce-product-gallery__trigger">🔍</a>
+#	<figure class="woocommerce-product-gallery__wrapper">
+#		<div data-thumb="https://distribuidoranico.cl/wp-content/uploads/2021/06/Captura-de-pantalla-234-100x100.png" data-thumb-alt="" class="woocommerce-product-gallery__image" style="position: relative; overflow: hidden;"><a href="https://distribuidoranico.cl/wp-content/uploads/2021/06/Captura-de-pantalla-234.png?v=cda73baae416"><img width="600" height="452" src="https://distribuidoranico.cl/wp-content/uploads/2021/06/Captura-de-pantalla-234.png" class="wp-post-image lazy-loaded" alt="" title="Captura de pantalla (234)" data-caption="" data-src="https://distribuidoranico.cl/wp-content/uploads/2021/06/Captura-de-pantalla-234.png" data-large_image="https://distribuidoranico.cl/wp-content/uploads/2021/06/Captura-de-pantalla-234.png" data-large_image_width="941" data-large_image_height="709" srcset="https://distribuidoranico.cl/wp-content/uploads/2021/06/Captura-de-pantalla-234-600x452.png 600w, https://distribuidoranico.cl/wp-content/uploads/2021/06/Captura-de-pantalla-234-300x226.png 300w, https://distribuidoranico.cl/wp-content/uploads/2021/06/Captura-de-pantalla-234-768x579.png 768w, https://distribuidoranico.cl/wp-content/uploads/2021/06/Captura-de-pantalla-234.png 941w" sizes="(max-width: 600px) 100vw, 600px"></a><img role="presentation" alt="" src="https://distribuidoranico.cl/wp-content/uploads/2021/06/Captura-de-pantalla-234.png" class="zoomImg" style="position: absolute; top: -148.568px; left: -475.139px; opacity: 0; width: 941px; height: 709px; border: none; max-width: none; max-height: none;"></div>	</figure>
+# </div>
+#<div class="summary entry-summary">
+#		<div class="woocommerce-product-details__short-description">
+#	<h1><strong>Rolls Fun Nuts</strong></h1>
+#</div>
+#<p class="price"><span class="woocommerce-Price-amount amount"><bdi><span class="woocommerce-Price-currencySymbol">$</span>3.234</bdi></span></p>
+#<div class="sml-single-cart-wrapp sml-single-product-outer"><p class="stock out-of-stock">Agotado</p>
+#<div class="sml-compare-wrapp"></div></div><div class="product_meta">
+#
+#	
+#	
+#		<span class="sku_wrapper">SKU: <span class="sku">252-1</span></span>
+#
+#	
+#	<span class="posted_in">Categoría: <a href="https://distribuidoranico.cl/product-category/chocolates/?v=cda73baae416" rel="tag">Chocolates</a></span>
+#	<span class="tagged_as">Etiquetas: <a href="https://distribuidoranico.cl/product-tag/chocolate/?v=cda73baae416" rel="tag">chocolate</a>, <a href="https://distribuidoranico.cl/product-tag/compartir/?v=cda73baae416" rel="tag">compartir</a>, <a href="https://distribuidoranico.cl/product-tag/costa/?v=cda73baae416" rel="tag">costa</a>, <a href="https://distribuidoranico.cl/product-tag/mani/?v=cda73baae416" rel="tag">mani</a>, <a href="https://distribuidoranico.cl/product-tag/rol/?v=cda73baae416" rel="tag">rol</a>, <a href="https://distribuidoranico.cl/product-tag/rols/?v=cda73baae416" rel="tag">rols</a></span>
+#	
+#</div>
+#
+#            <form action="" method="post" class="alert_wrapper">
+#                <input type="email" name="alert_email" id="alert_email" placeholder="Correo Electronico">
+#                <input type="hidden" name="alert_id" id="alert_id" value="559">
+#                <input type="submit" value="Contactame cuando haya stock" class="">
+#            </form> 
+#        	</div>
+
+# <div class="woocommerce-tabs wc-tabs-wrapper">
+#		<ul class="tabs wc-tabs" role="tablist">
+#							<li class="description_tab active" id="tab-title-description" role="tab" aria-controls="tab-description">
+#					<a href="#tab-description">
+#						Descripción					</a>
+#				</li>
+#					</ul>
+#					<div class="woocommerce-Tabs-panel woocommerce-Tabs-panel--description panel entry-content wc-tab" id="tab-description" role="tabpanel" aria-labelledby="tab-title-description" style="">
+#
+#	<h2>Descripción</h2>
+#
+# <p><strong>Alfajor Bon o Bon 40 g x5</strong></p>
+# <div class="g">
+# <div data-hveid="CB8QAA" data-ved="2ahUKEwisxI6M05fxAhWIIrkGHSUzBbgQFSgAMBh6BAgfEAA">
+# <div class="tF2Cxc">
+# <div class="IsZvec">
+# <div class="VwiC3b yXK7lf MUxGbd yDYNvb lyLwlc">Alfajor con relleno a base de leche y maní cubierto con chocolate con leche.</div>
+# <div></div>
+# <div>*PACK DE 5UNIDADES.</div>
+# </div>
+# <div id="eob_95" data-ved="2ahUKEwisxI6M05fxAhWIIrkGHSUzBbgQ2Z0BMBh6BAgfEAo"></div>
+# </div>
+# </div>
+# </div>
+# <div class="g">
+# <div data-hveid="CC0QAA" data-ved="2ahUKEwisxI6M05fxAhWIIrkGHSUzBbgQFSgAMBl6BAgtEAA"></div>
+# </div>
+#			</div>
+#
+#			</div>
+# <div class="woocommerce-tabs wc-tabs-wrapper">
+#		<ul class="tabs wc-tabs" role="tablist">
+#							<li class="description_tab active" id="tab-title-description" role="tab" aria-controls="tab-description">
+#					<a href="#tab-description">
+#						Descripción					</a>
+#				</li>
+#					</ul>
+#					<div class="woocommerce-Tabs-panel woocommerce-Tabs-panel--description panel entry-content wc-tab" id="tab-description" role="tabpanel" aria-labelledby="tab-title-description" style="">
+#
+#	<h2>Descripción</h2>
+#
+# <p><strong>Alfajor Bon o Bon 40 g x5</strong></p>
+# <div class="g">
+# <div data-hveid="CB8QAA" data-ved="2ahUKEwisxI6M05fxAhWIIrkGHSUzBbgQFSgAMBh6BAgfEAA">
+# <div class="tF2Cxc">
+# <div class="IsZvec">
+# <div class="VwiC3b yXK7lf MUxGbd yDYNvb lyLwlc">Alfajor con relleno a base de leche y maní cubierto con chocolate con leche.</div>
+# <div></div>
+# <div>*PACK DE 5UNIDADES.</div>
+# </div>
+# <div id="eob_95" data-ved="2ahUKEwisxI6M05fxAhWIIrkGHSUzBbgQ2Z0BMBh6BAgfEAo"></div>
+# </div>
+# </div>
+# </div>
+# <div class="g">
+# <div data-hveid="CC0QAA" data-ved="2ahUKEwisxI6M05fxAhWIIrkGHSUzBbgQFSgAMBl6BAgtEAA"></div>
+# </div>
+#			</div>
+#
+#			</div>
+
+
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+for i in range(1, 53):
+    url = 'https://distribuidoranico.cl/todos-los-productos/page/' + \
+        str(i)+'/?v=cda73baae416'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    productos = soup.find_all('div', class_='sml-product-title-wrapp')
+    for producto in productos:
+        # guardar en csv
+        #with open('productos.csv', 'a', newline='') as file:
+        #    writer = csv.writer(file)
+        #    # href es el atributo de la etiqueta
+        #    writer.writerow([producto.contents[0].contents[0]['href']])
+        # print(producto['href'])
+        # EXTRAER DETALLE PRODUCTO
+        url_producto = producto.contents[0].contents[0]['href']
+        page_producto = requests.get(url_producto)
+        soup_producto = BeautifulSoup(page_producto.content, 'html.parser')
+        # print(soup_producto.prettify())
+        # EXTRAER NOMBRE
+        nombre = soup_producto.find('div', class_='woocommerce-product-details__short-description').text
+        #eliminar saltos de linea
+        nombre = nombre.replace('\n', '')
+
+        # EXTRAER PRECIO
+        precio = soup_producto.find(
+            'span', class_='woocommerce-Price-amount amount').text
+        # eliminar saltos de linea
+        precio = precio.replace('\n', '')
+        # EXTRAER DESCRIPCION
+        #descripcion = soup_producto.find(
+        #    'div', class_='woocommerce-Tabs-panel woocommerce-Tabs-panel--description panel entry-content wc-tab').text
+        # GUARDAR EN CSV en una sola linea
+        data=[nombre, precio]
+        with open('productos.csv', 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(data)
+        print(nombre, precio)
+        
+
+
+
+
